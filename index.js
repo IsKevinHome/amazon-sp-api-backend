4f0695d0f8e42db643d18df82c1aaa25644468cd
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-03-03 23:39:56 -0800

    add callback

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001; // will be set by heroku
 const nodeEnv = process.env.NODE_ENV; // will be set by heroku
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI; // Must match the redirect URI in your app settings
 
 app.get('/', (req, res) => {
   res.send({
@@ -11,15 +16,38 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/api/stopserver', (req, res) => {
-  console.log('!!! Stopping server !!!');
-  process.exit(1);
-});
+// app.get('/api/stopserver', (req, res) => {
+//   console.log('!!! Stopping server !!!');
+//   process.exit(1);
+// });
 
-app.get('/api/oauth/amazon', (req, res) => {
-  console.log(req.query);
+app.get('/api/oauth/callback', async (req, res) => {
+  const code = req.query.spapi_oauth_code;
+  console.log('~~~code~~~', code);
+
+  try {
+    const tokenResponse = await axios.post(
+      'https://api.amazon.com/auth/o2/token',
+      querystring.stringify({
+        grant_type: 'authorization_code',
+        code,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+      })
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+    console.log('~~~logged access token~~~', accessToken);
+    res.send(`~~~Access Token~~~: ${accessToken}`);
+  } catch (error) {
+    console.error('Error:', error);
+    res
+      .status(500)
+      .send('Error exchanging authorization code for access token');
+  }
+
   res.send({
-    status: 'success',
     request: req.query,
   });
 });

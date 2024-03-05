651e252663c4aca4f8137b7af77b336937a960f8
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-03-04 22:03:57 -0800

    only log req.query

--- index.js
+++ index.js
@@ -16,40 +16,33 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.get('/api/stopserver', (req, res) => {
-//   console.log('!!! Stopping server !!!');
-//   process.exit(1);
-// });
-
 app.get('/api/oauth/callback', async (req, res) => {
-  const code = req.query.spapi_oauth_code;
-  console.log('~~~req.query~~~', req.query);
-
-  try {
-    const tokenResponse = await axios.post(
-      'https://api.amazon.com/auth/o2/token',
-      querystring.stringify({
-        grant_type: 'authorization_code',
-        code,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URI,
-      })
-    );
-
-    const accessToken = tokenResponse.data.access_token;
-    console.log('~~~logged access token~~~', accessToken);
-    res.send(`~~~Access Token~~~: ${accessToken}`);
-  } catch (error) {
-    console.error('Error:', error);
-    res
-      .status(500)
-      .send('Error exchanging authorization code for access token');
-  }
-
-  res.send({
-    request: req.query,
-  });
+  console.log(req.query);
+  //   const code = req.query.spapi_oauth_code;
+  //   console.log('~~~req.query~~~', req.query);
+  //   try {
+  //     const tokenResponse = await axios.post(
+  //       'https://api.amazon.com/auth/o2/token',
+  //       querystring.stringify({
+  //         grant_type: 'authorization_code',
+  //         code,
+  //         client_id: CLIENT_ID,
+  //         client_secret: CLIENT_SECRET,
+  //         redirect_uri: REDIRECT_URI,
+  //       })
+  //     );
+  //     const accessToken = tokenResponse.data.access_token;
+  //     console.log('~~~logged access token~~~', accessToken);
+  //     res.send(`~~~Access Token~~~: ${accessToken}`);
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //     res
+  //       .status(500)
+  //       .send('Error exchanging authorization code for access token');
+  //   }
+  //   res.send({
+  //     request: req.query,
+  //   });
 });
 
 app.listen(port, () => {
